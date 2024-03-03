d2cb082c17f073dcf87cbbb32680af8e51f9153c
kaju111 <saikiakaju275@gmail.com>
2024-03-03 19:13:39 +0530

    client

--- src/constants/color.js
+++ src/constants/color.js
@@ -2,3 +2,4 @@ export const orange = "#31656B";
 export const grayColor = "rgba(247,247,247,1)";
 export const lightblue = "#2694ab";
 export const grop = "#1F9E8B";
+export const matBlack = "#1c1c1c";
--- src/pages/Groups.jsx
+++ src/pages/Groups.jsx
@@ -1,8 +1,77 @@
-import React from "react";
-import { Grid } from "@mui/material";
-import { grop } from "../constants/color";
+import React, { useState } from "react";
+import {
+  Box,
+  Drawer,
+  Grid,
+  IconButton,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { grop, matBlack } from "../constants/color";
+import {
+  KeyboardBackspace as KeyboardBackspaceIcon,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 const Groups = () => {
+  const navigate = useNavigate();
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState();
+
+  const navigateBack = () => {
+    navigate("/");
+  };
+
+  const handleMobile = () => {
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+
+  const handleMobileClose = () => {
+    setIsMobileMenuOpen(false);
+  };
+
+  const IconBtns = (
+    <>
+      <Box
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+            position: "fixed",
+            right: "1rem",
+            top: "1rem",
+          },
+        }}
+      >
+        <Tooltip title="menu">
+          <IconButton onClick={handleMobile}>
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+
+      <Tooltip title="back">
+        <IconButton
+          sx={{
+            position: "absolute",
+            top: "2rem",
+            left: "2rem",
+            bgcolor: matBlack,
+            color: "white",
+            ":hover": {
+              bgcolor: "rgba(0,0,0,0.7)",
+            },
+          }}
+          onClick={navigateBack}
+        >
+          <KeyboardBackspaceIcon />
+        </IconButton>
+      </Tooltip>
+    </>
+  );
+
   return (
     <Grid container height={"100vh"}>
       <Grid
@@ -16,7 +85,7 @@ const Groups = () => {
         sm={4}
         bgcolor={grop}
       >
-        Groups List
+        <GroupList />
       </Grid>
       <Grid
         item
@@ -30,10 +99,35 @@ const Groups = () => {
           padding: "1rem 3rem",
         }}
       >
-        Grp details
+        {IconBtns}
       </Grid>
+
+      <Drawer
+        sx={{
+          display: {
+            xs: "block",
+            sm: "none",
+          },
+        }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileClose}
+      >
+        <GroupList w={"50vw"} />
+      </Drawer>
     </Grid>
   );
+};
+
+const GroupList = ({ w = "100%", myGroups = [], chatId }) => {
+  <Stack>
+    {myGroups.length > 0 ? (
+      myGroups.map((grop) => {})
+    ) : (
+      <Typography textAlign={"center"} padding="1rem">
+        No groups
+      </Typography>
+    )}
+  </Stack>;
 };
 
 export default Groups;
